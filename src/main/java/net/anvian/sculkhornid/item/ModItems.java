e56f7bfc0d36c8e69e5fa605862d2b68d313240b
Andre Carbajal <andre.carbajal.vargas@gmail.com>
2023-02-12 12:12:46 -0500

    renamed classes

--- src/main/java/net/anvian/sculkhornid/item/ModItems.java
+++ src/main/java/net/anvian/sculkhornid/item/ModItems.java
@@ -1,8 +1,8 @@
 package net.anvian.sculkhornid.item;
 
 import net.anvian.sculkhornid.SculkHornMod;
-import net.anvian.sculkhornid.item.custom.SculkHorn;
-import net.anvian.sculkhornid.item.custom.SculkHornSonicBoom;
+import net.anvian.sculkhornid.item.custom.SculkHornArea;
+import net.anvian.sculkhornid.item.custom.SculkHornDistance;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
@@ -10,14 +10,14 @@ import net.minecraft.util.Rarity;
 import net.minecraft.util.registry.Registry;
 
 public class ModItems {
-    public static final Item SCULKHORN =registerItem("sculkhorn", new SculkHorn(
+    public static final Item SCULKHORN =registerItem("sculkhorn", new SculkHornArea(
             new FabricItemSettings()
                     .rarity(Rarity.EPIC)
                     .maxCount(1)
                     .maxDamage(SculkHornMod.CONFIG.AREA_DURABILITY())//350
                     .group(ModItemGroup.SCULKHORNGROUP)));
 
-    public static final Item SCULKHORN_SONICBOOM = registerItem("sculkhorn_sonicboom", new SculkHornSonicBoom(
+    public static final Item SCULKHORN_SONICBOOM = registerItem("sculkhorn_sonicboom", new SculkHornDistance(
             new FabricItemSettings()
                     .rarity(Rarity.EPIC)
                     .maxCount(1)
